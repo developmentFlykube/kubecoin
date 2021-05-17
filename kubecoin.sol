ca1279203ca1eb5d1cb3823861ecbfacf1f2f489
Roger <roger.solis@flykube.com>
2021-05-17 11:52:13 +0200

    First commit.

---
+++ kubecoin.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.4;
+
+import './libraries/IERC20.sol';
+import './libraries/Context.sol';
+import './libraries/Ownable.sol';
+
+contract KubeCoin is Context, IERC20, Ownable {
+
+
+}
