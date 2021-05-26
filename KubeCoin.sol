ab16fa1123744d376f31f21b67fce0046d840bcd
Roger <roger.solis@flykube.com>
2021-05-26 18:48:43 +0200

    Adapted to openzeppelin ERC20 implementation.

--- KubeCoin.sol
+++ KubeCoin.sol
@@ -2,146 +2,23 @@
 
 pragma solidity ^0.8.4;
 
-import './IBEP20.sol';
-import './Context.sol';
-import './Ownable.sol';
-import './SafeMath.sol';
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
-contract NewAgeToken is Context, IBEP20, Ownable {
+contract NewAgeToken is ERC20PresetFixedSupply {
 
-    using SafeMath for uint256;
-
-    string private _name;
-    string private _symbol;
     uint8 private _decimals;
-    uint256 private _totalSupply;
-    mapping (address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;
 
-    constructor() {
-        _name = "NewAgeToken";
-        _symbol = "NEWAGE";
+    constructor() ERC20PresetFixedSupply("NewAgeToken", "NEWAGE", 360 * 10**6 * 10**uint(_decimals), msg.sender) {
         _decimals = 2;
-        _totalSupply = 360 * 10**6 * 10**uint(_decimals);
-        _balances[owner()] = _totalSupply;
-        emit Transfer(address(0), owner(), _totalSupply);
     }
 
-    /* BEP20 IMPLEMENTATION METHODS */
-
-    function name() external view override returns (string memory) {
-        return _name;
-    }
-
-    function symbol() external view override returns (string memory) {
-        return _symbol;
-    }
-
-    function decimals() external view override returns (uint8) {
-        return _decimals;
-    }
-
-    function totalSupply() external view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) external view override returns (uint256) {
-        return _balances[account];
-    }
-
-    function getOwner() external view override returns (address) {
+    // BEP20 mandatory method.
+    function getOwner() external view returns (address) {
         return owner();
     }
 
-    function transfer(address recipient, uint256 amount) external override returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
-        return true;
-    }
-
-    function allowance(address owner, address spender) external view override returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-    function approve(address spender, uint256 amount) external override returns (bool) {
-        _approve(_msgSender(), spender, amount);
-        return true;
-    }
-
-    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
-        _transfer(sender, recipient, amount);
-        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "Transfer amount exceeds allowance"));
-        return true;
-    }
-
-    /* NON ERC-20 STANDARD METHODS */
-
-    function burn(uint256 amount) external returns (bool) {
-        _burn(_msgSender(), amount);
-        return true;
-    }
-
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
-        return true;
-    }
-
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
-        uint256 currentAllowance = _allowances[_msgSender()][spender];
-        require(currentAllowance >= subtractedValue, "Decreased allowance below zero");
-        unchecked {
-            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
-        }
-        return true;
-    }
-
-    /* PRIVATE METHODS */
-
-    function _approve(address owner, address spender, uint256 value) private {
-        require(owner != address(0), "Cannot approve from the zero address");
-        require(spender != address(0), "Cannot approve to the zero address");
-        _allowances[owner][spender] = value;
-        emit Approval(owner, spender, value);
-    }
-
-    function _transfer(address from, address to, uint256 amount) private {
-        require(from != address(0), "Cannot transfer from the zero address");
-        require(to != address(0), "Cannot transfer to the zero address");
-        require(amount > 0, "Transfer amount must be greater than zero");
-        _balances[from] = _balances[from].sub(amount, "Transfer amount exceeds balance");
-        _balances[to] = _balances[to].add(amount);
-        emit Transfer(from, to, amount);
-    }
-
-    function _burn(address account, uint256 amount) private {
-        require(account != address(0), "Cannot burn from the zero address");
-        _balances[account] = _balances[account].sub(amount, "Burn amount exceeds balance");
-        _totalSupply = _totalSupply.sub(amount);
-        emit Transfer(account, address(0), amount);
+    // We override default 18 decimals from openzeppelin
+    function decimals() external view override returns (uint8) {
+        return _decimals;
     }
 }
