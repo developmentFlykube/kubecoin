c4357db568cdfc25f7ac4539388a416fdeac4d00
Roger <roger.solis@flykube.com>
2021-05-26 19:03:47 +0200

    Fixed compile bugs.

--- KubeCoin.sol
+++ KubeCoin.sol
@@ -7,18 +7,20 @@ import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 contract NewAgeToken is ERC20PresetFixedSupply {
 
     uint8 private _decimals;
+    address _owner;
 
     constructor() ERC20PresetFixedSupply("NewAgeToken", "NEWAGE", 360 * 10**6 * 10**uint(_decimals), msg.sender) {
         _decimals = 2;
+        _owner = msg.sender;
     }
 
     // BEP20 mandatory method.
     function getOwner() external view returns (address) {
-        return owner();
+        return _owner;
     }
 
     // We override default 18 decimals from openzeppelin
-    function decimals() external view override returns (uint8) {
+    function decimals() public view virtual override returns (uint8) {
         return _decimals;
     }
 }
